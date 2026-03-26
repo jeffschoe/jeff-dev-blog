2e1e33b6ec88c465dddf13903a6ecccc474a98ae
jeffschoe <ratacame@protonmail.com>
2026-03-25 20:08:37 -0400

    Added link to Blog page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -33,6 +34,10 @@ export default function Home() {
             </a>{" "}
             center.
           </p>
+        </div>
+        <div>
+          <Link href="/blog">View my Blog</Link>
+
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
