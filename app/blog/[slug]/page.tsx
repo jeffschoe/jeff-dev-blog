b6f5af5073e1de0ed450426eced228c22228055e
jeffschoe <ratacame@protonmail.com>
2026-03-26 09:45:55 -0400

    Added metadata function for post titles. Removed manual html insertion of post titles.

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,6 +3,24 @@ import { getAllPosts } from "@/lib/posts";
 import Link from "next/link"; 
 
 
+//sets title metadata on each page
+export async function generateMetadata({ params }: { params: Promise<{ slug: string }>}) {
+  const { slug } = await params; // destructuring
+  const posts =  getAllPosts();
+  const post = posts.find((p) => p.slug === slug); //returns the first post where the post.slug === param.slug. slug = markdown file name, like: "my-first-post.md" → slug = "my-first-post"
+  if (!post) {
+    return {
+      title: "Post Not Found",
+    };
+  }
+
+  return {
+    title: post.title,
+  }
+
+
+}
+
 export default async function PostPage({ params }: { params: Promise<{ slug: string }> }) {
   // incoming arg must be of object shape: { params: { slug: string } } 
   // directly destructure the incoming argument, "{ params }". 
@@ -19,7 +37,6 @@ export default async function PostPage({ params }: { params: Promise<{ slug: str
 
   return  (
     <div>
-      <title>{post.title}</title>
       <h1 className="text-2xl font-bold mb-4">{post.title}</h1>
       <p className="text-sm mb-4 text-gray-500">{post.date}</p>
       <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
